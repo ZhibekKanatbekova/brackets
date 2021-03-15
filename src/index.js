97ed3c83ce409648d533f7a314a04346bd98e42d
Zhibek <jiko633@gmail.com>
2021-03-15 23:44:12 +0400

    fix: half sold

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let brackets = str;
+ let stack = [];
+
+ for (let bracket of bracketsConfig) {
+     let bracketsIndex = brackets.indexOf(bracket);
+
+     if (bracketsIndex === -1) {
+         continue;
+     }
+
+     if (bracketsIndex % 2 === 0) {
+         stack.push(bracketsIndex + 1);
+     } else {
+         if (stack.pop() !== bracketsIndex) {
+             return true;
+         }
+     }
+ }
+ return true
 }
